95b2e6d414da81404f5fb3544263d15c25db83ec
nur.rahmat <nur.rahmat@aksaramaya.com>
2020-09-15 16:06:34 +0700

    Add verybigsum class and refactor previous class

---
+++ src/main/kotlin/nrahmatd/hackerrank/verybigsum/VeryBigSum.kt
@@ -0,0 +1,24 @@
+package nrahmatd.hackerrank.verybigsum
+
+import java.util.Scanner
+
+// Complete the aVeryBigSum function below.
+fun aVeryBigSum(ar: Array<Long>): Long {
+    return ar.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    println("===Sample Input===\n" +
+            "5\n" +
+            "1000000001 1000000002 1000000003 1000000004 1000000005")
+    println("Input number : ")
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
