fde3acbbfb878b328e1631f2f6e74e66e02b9993
Nur Rahmat Dwi Riyanto <nrahmatd@gmail.com>
2023-08-20 23:29:11 +0700

    Solve Big Sort and Add replace problem statement image file with pdf file

---
+++ src/main/kotlin/nrahmatd/hackerrank/11_big_sort/BigSort.kt
@@ -0,0 +1,33 @@
+package nrahmatd.hackerrank.`11_big_sort`
+
+/*
+ * Complete the 'bigSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
+ */
+
+fun bigSorting(unsorted: Array<String>): Array<String> {
+    // Write your code here
+    val ints = unsorted.map { it.toBigInteger() }.toTypedArray()
+
+    ints.sort()
+
+    val strs = ints.map { it.toString() }.toTypedArray()
+
+    return strs
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val unsorted = Array<String>(n, { "" })
+    for (i in 0 until n) {
+        val unsortedItem = readLine()!!
+        unsorted[i] = unsortedItem
+    }
+
+    val result = bigSorting(unsorted)
+
+    println(result.joinToString("\n"))
+}
